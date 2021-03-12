7e52ee660ce50ed745352982c9a5c43d129e712d
kathrync23 <kathrynclark16@augustana.edu>
2021-03-12 09:16:40 -0600

    added some margins and breaks on register screen

--- instead-it/src/User/Register.tsx
+++ instead-it/src/User/Register.tsx
@@ -1,35 +1,38 @@
 import React, { SyntheticEvent } from 'react';
 
-
 function RegisterComponent() {
-    function handleFormInput(e: SyntheticEvent) {
+  function handleFormInput(e: SyntheticEvent) {}
 
-    }
+  function submitForm() {}
 
-    function submitForm() {
-
-    }
-
-
-    return (
-        <div>
-            <form>
-                <label>
-                    Username:
-                    <input type='text' name='username' onChange={handleFormInput}/>
-                </label>
-                <label>
-                    Password:
-                    <input type='password' name='password' onChange={handleFormInput}/>
-                </label>
-                <label>
-                    Verify Password:
-                    <input type='password' name='verifyPassword' onChange={handleFormInput}/>
-                </label>
-                <button className='btn-btn-submit' onClick={submitForm}>Register</button>
-            </form>
-        </div>
-    )
+  return (
+    <div className='form-input'>
+      <form>
+        <label>
+          Username:
+          <input type='text' name='username' onChange={handleFormInput} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type='password' name='password' onChange={handleFormInput} />
+          <br />
+        </label>
+        <label>
+          Verify Password:
+          <input
+            type='password'
+            name='verifyPassword'
+            onChange={handleFormInput}
+          />
+        </label>
+        <br />
+        <button className='btn-btn-submit' onClick={submitForm}>
+          Register
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default RegisterComponent;
